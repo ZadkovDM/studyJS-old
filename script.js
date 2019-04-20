1f64a48733979affd59291e9cf5db549355a6f25
Dmitriy <gaucho92@mail.ru>
2019-04-20 18:20:05 +0200

    Наш третий коммит

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 'use strict'
 
-let money = +prompt("Ваш бюджет на месяц?");
-
-console.log(typeof (money));
-
-
-let time = +prompt("Введите дату в формате YYYY-MM-DD");
-
-console.log(typeof (time));
+let money = +prompt("Ваш бюджет на месяц?"),
+	time = +prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-	budget: (money),
-	timeData: (time),
+	budget: money,
+	timeData: time,
 	expenses: {},
 	optionalExpenses: {},
-	income: {},
+	income: [],
 	savings: false
 };
 
-let nameCategory = prompt("Введите обязательную статью расходов в этом месяце"),
-typeCategory = +prompt("Во сколько обойдется?");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+	a2 = +prompt("Во сколько обойдется?"),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+	a4 = +prompt("Во сколько обойдется?");
 
-appData.expenses[nameCategory] = typeCategory;
-
-nameCategory = prompt("Введите обязательную статью расходов в этом месяце"),
-typeCategory = +prompt("Во сколько обойдется?");
-
-appData.expenses[nameCategory] = typeCategory;
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
 
 console.log(appData);
 
